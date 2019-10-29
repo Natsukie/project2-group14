93fd8fdb20b9f3a37121c5c5c8f05f7c3dffd32c
Natsukie <aproditeloveu@gmail.com>
2019-10-29 06:07:30 -0500

    final verson

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PullsComponent } from './pulls.component';
+import { HomeComponent } from './home.component';
 
-describe('PullsComponent', () => {
-  let component: PullsComponent;
-  let fixture: ComponentFixture<PullsComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PullsComponent ]
+      declarations: [ HomeComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PullsComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
